9c59ff54e0f55680790ebf9691475e07da2c593d
Khanyisile Buthelezi <thandofatimabuthelezi@gmail.com>
2024-09-28 22:31:37 +0200

    Populated the Add Jobs Components Linked the Add job page to Add Job Option on the Home Page Linked the Home Page to theReact App Page

--- frontend/group5-react-app/src/Components/Home/Home.js
+++ frontend/group5-react-app/src/Components/Home/Home.js
@@ -1,20 +1,25 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Link, Route } from 'react-router-dom';
 import "./Components/Home.css";
-
+import "./Components/AddJob.js";
 const Home = () => {
   return (
     <div className="home-container">
       <h1>Welcome to our Job Listing Platform</h1>
       <nav>
+        
+            <h3>Select option below</h3>
         <ul className="nav-list">
           <li>
+          <Router>
             <Link
-              to="/add-job"
+              to="./Component/Add Job"
               aria-label="Add New Job"
             >
               Add New Job
             </Link>
+            
+            </Router>
           </li>
           <li>
             <Link
@@ -24,10 +29,13 @@ const Home = () => {
               View Job Listings
             </Link>
           </li>
+          <Routes>
+            <Route path="./Component/Add Job" element = {<AddJob />} />
+            </Routes>
         </ul>
+        
       </nav>
     </div>
   );
 };
-
 export default Home;
